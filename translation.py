d6d9d36d84a9af3117ed32ba4082e41ebcf14ea7
hanafir <fahrenzah@gmail.com>
2020-09-15 18:26:05 +0700

    Update translation.py

--- translation.py
+++ translation.py
@@ -28,7 +28,7 @@ Type /help for more details."""
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
-    CUSTOM_CAPTION_UL_FILE = " <b>Join for more | @Editor_Indonesia | APK MOD INDONESIA</b> "
+    CUSTOM_CAPTION_UL_FILE = " | <b>Join for more | @Editor_Indonesia | APK MOD INDONESIA</b> | "
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     HELP_USER = """Hai <b><i>{}</i></b>, 
