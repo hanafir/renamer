4ad12c8e4d08cd543f600033510092359d80ad3a
hanafir <fahrenzah@gmail.com>
2020-09-15 13:44:15 +0700

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -53,7 +53,7 @@ async def rename_doc(bot, update):
                 )
             )
             return
-        description =file_name + Translation.CUSTOM_CAPTION_UL_FILE
+        description =file_name +file_name + Translation.CUSTOM_CAPTION_UL_FILE
         download_location = Config.DOWNLOAD_LOCATION + "/"
         a = await bot.send_message(
             chat_id=update.chat.id,
